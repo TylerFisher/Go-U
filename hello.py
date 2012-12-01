678132fe5e56d89c32aaa2c15972e747c503fab9
Tyler Fisher <bassist201@gmail.com>
2012-12-01 13:02:56 -0600

    a little reconfiguration based on some Flask OAuth examples

--- hello.py
+++ hello.py
@@ -7,22 +7,30 @@ app = Flask(__name__)
 url_for('static', filename='stylesheets/screen.css')
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    resp = twitter.get('search/tweets.json?q=B1GCats')
+        if resp.status == 200:
+            tweets = resp.data
+        else:
+            flash('Unable to load tweets from Twitter. Maybe out of '
+                  'API calls or Twitter is overloaded.')
+    return render_template('index.html', tweets=tweets)
 
 if __name__ == '__main__':
     app.run(debug=True)
 
+oauth = OAuth()
+
+twitter = oauth.remote_app('twitter',
+    base_url='https://api.twitter.com/1.1/',
+    request_token_url='https://api.twitter.com/oauth/request_token',
+    access_token_url='https://api.twitter.com/oauth/access_token',
+    authorize_url='https://api.twitter.com/oauth/authenticate',
+    consumer_key='IOvCZlOF3IHPqciM5tbDEA',
+    consumer_secret='qLBqyus7BiEqm88cx08cnjBme0Dye1oemM2XdXtmKs'
+)
+
 def twitter_call():
-    oauth = OAuth()
-    twitter = oauth.remote_app('twitter',
-        base_url='https://api.twitter.com/1.1/',
-        request_token_url='https://api.twitter.com/oauth/request_token',
-        access_token_url='https://api.twitter.com/oauth/access_token',
-        authorize_url='https://api.twitter.com/oauth/authenticate',
-        consumer_key='IOvCZlOF3IHPqciM5tbDEA',
-        consumer_secret='qLBqyus7BiEqm88cx08cnjBme0Dye1oemM2XdXtmKs'
-    )
 
     @twitter.tokengetter
     def get_twitter_token(token=None):
